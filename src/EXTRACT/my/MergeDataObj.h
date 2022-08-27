d9085769525b2cdcebaed262d9f8c3ebe3f5872d
Ilya Mikhalin <thebear@yandex.ru>
2022-08-27 20:22:43 +0300

    Пока закончил с анализом окошка с результатами мержа, далее буду пытаться разделить KDiff3App

--- src/EXTRACT/my/MergeDataObj.h
+++ src/EXTRACT/my/MergeDataObj.h
@@ -33,6 +33,16 @@ class KDiff3App;
 
 class MergeDataObj
 {
+    // Использование - сначала инициализация - Создание, init_setOptions(), init()
+    // Далее вызов merge() для мержа из диффов и решения конфликтов
+    // На этом моменте внутренние данные заполняются чем-то и получается состояние окошка мержа после прохода авто-мержа первоначального (с авто разрешением конфликтов или без в зависимости от того был ли флаг).
+    // Далее можно вызывать choose() для разрешения конфликтов на одной из строчек (текущая строчка находится в m_currentMergeLineIt - надо еще понять как ее програмно изменять правильно).
+    // choose() из ui дергается при нажатии на кнопку выбора какую строчку посдтавить (A,B,C) для решения конфликта.
+    // Еще каким то образом можно вроде просто текст редактирвоать поверх уже решенных конфликтов - надо тоже найти как это делается из UI и тут это сделать видимо.
+    // После того как все конфликты решены с помощью рук (choose() и явное редактирование текста) - можно сохранить результаты мержа в файл.
+    // сохранения тут нету, но есть получение данных для сохранения в виде байтов файла - это getOutFileData()
+
+
   public:
     MergeDataObj();
 
@@ -46,22 +56,25 @@ class MergeDataObj
         const Diff3LineList* pDiff3LineList,
         TotalDiffStatus* pTotalDiffStatus);
 
+    // Процесс мержа с возможностью авто-мержа. После этого действия идет ручное решение конфликтов.
+    void merge(bool bAutoSolve, e_SrcSelector defaultSelector, bool bConflictsOnly, bool bWhiteSpaceOnly, std::function<void()> callback1, std::function<void()> callback2);
+
+    // Выбрать один из вариантов решения конфликта на текущей строчке (текущая строчка сидит в m_currentMergeLineIt)
+    void choose(e_SrcSelector selector);
+
+    // Проходится по данным m_mergeLineList и считает кол-во конфликтов
+    int getNrOfUnsolvedConflicts(int* pNrOfWhiteSpaceConflicts = nullptr);
+
+    // Выдает представление m_mergeLineList в виде итогового файла (смерженный файл) в виде out буффера.
+    bool getOutFileData(QTextCodec* pEncoding, e_LineEndStyle eLineEndStyle, void* &out_pBuffer, qint64 &out_bufferLength);
+  public:
+
     void reset();
 
     inline void clearMergeList()
     {
         m_mergeLineList.clear();
     }
-
-    // Выдает представление m_mergeLineList в виде итогового файла (смерженный файл) в виде out буффера.
-    bool getOutFileData(QTextCodec* pEncoding, e_LineEndStyle eLineEndStyle, void* &out_pBuffer, qint64 &out_bufferLength);
-
-    // Проходится по данным m_mergeLineList и считает кол-во конфликтов
-    int getNrOfUnsolvedConflicts(int* pNrOfWhiteSpaceConflicts = nullptr);
-
-    void choose(e_SrcSelector selector);
-
-    void merge(bool bAutoSolve, e_SrcSelector defaultSelector, bool bConflictsOnly, bool bWhiteSpaceOnly, std::function<void()> callback1, std::function<void()> callback2);
 
     // Получить строку по номеру строки в итоговом мерже (либо пустая строка в случае неудачи)
     QString getString(int lineIdx);
